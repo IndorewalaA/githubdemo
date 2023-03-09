842f08c6b23a07c524a5b0b95c205aae8a95f4f5
IndorewalaA <abdul.indorewala@ufl.edu>
2023-03-09 12:12:59 -0500

    commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def encode():
+
+
+def main():
+    startDeterminer = True
+    while startDeterminer:
+        print("Menu", "-------------", "1. Encode", "2. Decode", "3. Quit")
+
+
+if __name__ == '__main__':
+    main()
+
