01f84f647f5117da18f9e102003f4513727c0e5d
IndorewalaA <abdul.indorewala@ufl.edu>
2023-03-09 12:31:16 -0500

    commit

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-def encode():
+def encode(password):
+    encoded = ""
+    for digit in range(len(password)):
+        endig = int(password[digit]) + 3
+        encoded += str(endig)
+    return encoded
 
 
 def main():
     startDeterminer = True
+    encodedPass = ""
     while startDeterminer:
-        print("Menu", "-------------", "1. Encode", "2. Decode", "3. Quit")
-
+        print("Menu", "-------------", "1. Encode", "2. Decode", "3. Quit", sep="\n")
+        print()
+        userChoice = int(input("Please enter an option: "))
+        if userChoice == 1:
+            password = input("Please enter your password to encode: ")
+            encodedPass = encode(password)
+            print("Your password has been encoded and stored! ")
+            print()
+        elif userChoice == 3:
+            break
 
 if __name__ == '__main__':
     main()
