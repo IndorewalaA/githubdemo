6a1ba75d35d07e9e87df8878df83821b22d49693
mrjaq <dorsettj@ufl.edu>
2023-03-09 12:49:11 -0500

    I added decode

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ def encode(password):
         encoded += str(endig)
     return encoded
 
-
+def decode(password):
+    encoded = ""
+    for digit in range(len(password)):
+        endig = int(password[digit]) - 3
+        encoded += str(endig)
+    return encoded
 def main():
     startDeterminer = True
     encodedPass = ""
+    password = ""
     while startDeterminer:
         print("Menu", "-------------", "1. Encode", "2. Decode", "3. Quit", sep="\n")
         print()
@@ -18,6 +24,9 @@ def main():
             encodedPass = encode(password)
             print("Your password has been encoded and stored! ")
             print()
+        elif userChoice == 2:
+            decodedPass = decode(password)
+            print(f"The encoded password is {encodedPass}, and the original password is {decodedPass}. ")
         elif userChoice == 3:
             break
 
